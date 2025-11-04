8dc7707629a642be49e0349de7fc4ed1e60defc2
teknoziraat <teknoziraattoplulugu@gmail.com>
2025-11-04 23:53:38 +0300

    ilk commit

---
+++ site.js
@@ -0,0 +1,28 @@
+
+
+
+const menuLink = document.getElementById('tabbt');
+const tab = document.getElementById('tab')
+
+  menuLink.addEventListener('click', (e) => {
+    e.preventDefault();          // prevent link navigation
+    tab.classList.toggle('active'); // show/hide tab
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
